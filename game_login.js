e2593282a97b7f6257f15276d77d54a91e753ded
Sinliszt <86060857+Sinliszt@users.noreply.github.com>
2022-04-15 15:04:49 +0400

    Delete game_login.js

--- game_login.js
+++
@@ -1,7 +0,0 @@
-function addUser(){
-    var player_name1 = document.getElementById("player1user").value;
-    var player_name2 = document.getElementById("player2user").value;
-    localStorage.setItem("player_name1", player_name1);
-    localStorage.setItem("player_name2", player_name2);
-    window.location.replace("quiz_game.html")
-}
